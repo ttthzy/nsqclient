0dc0c13a5d9e4af95b69788767f007536087c36e
ttthzy <80186658@qq.com>
2015-12-28 11:34:09 +0800

    eason

--- controller/MainController.go
+++ controller/MainController.go
@@ -1,11 +1,7 @@
 package controller
 
 import (
-	"encoding/json"
-	"io"
-	"log"
-	"net/http"
-	"text/template"
+
 )
 
 type BaseJsonBean struct {
@@ -16,59 +12,4 @@ type BaseJsonBean struct {
 
 func NewBaseJsonBean() *BaseJsonBean {
 	return &BaseJsonBean{}
-}
-
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		http.Redirect(w, r, "/home.html", http.StatusFound)
-	}
-
-	t, err := template.ParseFiles("template/html/404.html")
-	if err != nil {
-		log.Println(err)
-	}
-	t.Execute(w, nil)
-
-}
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("template/html/home.html")
-	if err != nil {
-		log.Println(err)
-	}
-	t.Execute(w, nil)
-}
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("template/html/index.html")
-	if err != nil {
-		log.Println(err)
-	}
-	t.Execute(w, nil)
-}
-func GetNsqHandler(w http.ResponseWriter, req *http.Request) {
-	//获取客户端通过GET/POST方式传递的参数
-	req.ParseForm()
-	param1, found1 := req.Form["contype"]
-	result := NewBaseJsonBean()
-
-	if !found1 {
-		result.Code = -99
-		result.Message = "请勿非法访问"
-	}
-
-	contype := param1[0]
-
-	if contype == "start" {
-		result.Code = 100
-		result.Message = "与服务器连接成功"
-		//lib.Connect_Nsq()
-	} else {
-		result.Code = 101
-		result.Message = "与服务器断开"
-		//lib.Cmdstp()
-	}
-
-	//向客户端返回JSON数据
-	bytes, _ := json.Marshal(result)
-	io.WriteString(w, string(bytes))
-
 }
