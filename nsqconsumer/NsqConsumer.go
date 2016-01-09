468b48757005d2aed824eb860f1ebe89219b24bf
jessehua <jessehua2009@qq.com>
2016-01-09 17:43:21 +0800

    消息持久化

--- nsqconsumer/NsqConsumer.go
+++ nsqconsumer/NsqConsumer.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-    "nsqclient/lib"
-	"nsqclient/models"
-    "github.com/nsqio/go-nsq"
 	"fmt"
-	"time"
+	"nsqclient/lib"
+	"nsqclient/models"
 	"sync"
+	"time"
+
+	"github.com/nsqio/go-nsq"
 )
 
-const(
-    NSQCONSTR string="nsq-ttthzygi35.tenxcloud.net:40255"
+const (
+	NSQCONSTR string = "nsq-ttthzygi35.tenxcloud.net:40255"
 )
 
-type NsqMsgHandler struct{
-    msg models.Messages
-    waitGroup sync.WaitGroup
+type NsqMsgHandler struct {
+	msg       models.Messages
+	waitGroup sync.WaitGroup
 }
 
 ///NSQ消息处理
-func (handler *NsqMsgHandler)HandleMessage(nsqMsg *nsq.Message) error{
-    handler.waitGroup.Add(1)
-    defer handler.waitGroup.Done()
+func (handler *NsqMsgHandler) HandleMessage(nsqMsg *nsq.Message) error {
+	handler.waitGroup.Add(1)
+	defer handler.waitGroup.Done()
 
-    handler.msg.Message=lib.DecodeStr(string(nsqMsg.Body))
-    handler.msg.MessageID=string(nsqMsg.ID[:])
-    handler.msg.SendDate=time.Now()
-    fmt.Println("Got msg:",handler.msg.MessageID,handler.msg.Message)
-    return nil
+	handler.msg.Message = lib.DecodeStr(string(nsqMsg.Body))
+	handler.msg.MessageID = string(nsqMsg.ID[:])
+	handler.msg.SendDate = time.Now()
+	fmt.Println("Got msg:", handler.msg.MessageID, handler.msg.Message)
+	
+    return saveMsg(handler.msg)
+}
+
+func saveMsg(msg models.Messages) error{
+    fileHelper:=new(lib.FileHelper)
+    
+    
+    return fileHelper.WriteFile("data.txt",msg.Message)
 }
 
 func main() {
@@ -36,24 +45,23 @@ func main() {
 		Channel: "Jessehua",
 		UserID:  "00001",
 	}
-    
-    config := nsq.NewConfig()
-    consumer, err := nsq.NewConsumer(nci.Topic, nci.Channel, config)
+
+	config := nsq.NewConfig()
+	consumer, err := nsq.NewConsumer(nci.Topic, nci.Channel, config)
 	if err != nil {
-        fmt.Println("NSQ消费者创建失败！")
+		fmt.Println("NSQ消费者创建失败！")
 		return
 	}
-    
-    handler:=new(NsqMsgHandler)
-    handler.waitGroup.Add(1)
-    
-    consumer.AddHandler(nsq.HandlerFunc(handler.HandleMessage))
-    conErr:=consumer.ConnectToNSQD(NSQCONSTR)
-    if conErr!=nil{
-        fmt.Println("NSQ服务器连接失败")
-        return
-    }
-    
-    handler.waitGroup.Wait()
-}
 
+	handler := new(NsqMsgHandler)
+	handler.waitGroup.Add(1)
+
+	consumer.AddHandler(nsq.HandlerFunc(handler.HandleMessage))
+	conErr := consumer.ConnectToNSQD(NSQCONSTR)
+	if conErr != nil {
+		fmt.Println("NSQ服务器连接失败")
+		return
+	}
+
+	handler.waitGroup.Wait()
+}
